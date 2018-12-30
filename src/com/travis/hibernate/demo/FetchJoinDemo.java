c6175c6ba042d24e3b5fd83ab91e45c7d0f77a09
qzy <weelgod@gmail.com>
2018-12-30 19:18:50 +0800

    237 HQL fetch

--- src/com/travis/hibernate/demo/FetchJoinDemo.java
+++ src/com/travis/hibernate/demo/FetchJoinDemo.java
@@ -6,8 +6,9 @@ import com.travis.hibernate.demo.entity.InstructorDetail;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
-public class EagerLazyDemo {
+public class FetchJoinDemo {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
@@ -18,7 +19,12 @@ public class EagerLazyDemo {
 
         try (Session session = factory.getCurrentSession(); factory) {
             session.beginTransaction();
-            Instructor instructor = session.get(Instructor.class, 5);
+            Query<Instructor> query = session.createQuery("select i from Instructor i " +
+                            "join fetch i.courses " +
+                            "where i.id=:theInstructorId",
+                    Instructor.class);
+            query.setParameter("theInstructorId", 5);
+            Instructor instructor = query.getSingleResult();
             System.out.println(instructor);
             session.getTransaction().commit();
             System.out.println(instructor.getCourses());
